3f3b53e820c5735b1909b27a872d7540e5eb1f64
Brandon Kim <kimbrandon97@gmail.com>
2023-04-16 00:27:16 -0700

    Linked List Cycle (141) solution

---
+++ 141.cpp
@@ -0,0 +1,21 @@
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) return false;
+        ListNode* slow = head, *fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (fast == slow) return true;
+        }
+
+        return false;
+    }
+};
